83e8af2309d0ca714943e3f45eb5b562b85692a3
jcho42 <jason.cho.301230@gmail.com>
2020-10-12 16:22:56 -0500

    feat: moved homescreen to its own screen and removed back button on homescreen

--- client-side/App.js
+++ client-side/App.js
@@ -1,24 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Provider } from 'react-redux'
-import { Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import store from './store'
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import store from './store';
 import Chat from './screens/Chat';
-import {Login} from './screens'
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Chat"
-        onPress={() => navigation.navigate('Chat')}
-      />
-    </View>
-  );
-}
+import { Login, HomeScreen } from './screens';
 
 const Stack = createStackNavigator();
 
@@ -27,8 +13,14 @@ function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Login" component= {Login} />
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={() => ({
+              headerLeft: null,
+            })}
+          />
           <Stack.Screen name="Chat" component={Chat} />
         </Stack.Navigator>
       </NavigationContainer>
