bfdf7b97a0d5efc2110c24c9ef0db435a2e6c9f1
HoraceZ <zhanghorace@gmail.com>
2020-10-12 16:44:00 -0400

    chore: re-commit

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Git workflow
 7. Assign reviewer
 8. Reviewer will review and click “Add your review”, “approve” , “submit review”, “merge pull request”
 9. Delete branch if no longer needed
+
+
+Git workflow on branch instead of master:
+1. Pull: git checkout -b *new_branch_name* origin/*original-branch-name*
+2. Push: git push origin HEAD:*original-branch-name*
--- client-side/screens/Chat.js
+++ client-side/screens/Chat.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import io from 'socket.io-client'
 
 export default class Chat extends Component {
+  componentDidMount() {
+    const socket = io('http://localhost:7070', {
+      transports: ['websocket'], jsonp: false
+    });
+    socket.connect();
+    socket.on('connect', () => {
+      console.log('connected to socket server');
+    });
+  }
+
   render() {
     return (
       <View>
