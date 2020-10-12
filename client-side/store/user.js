9cde7262a5f88ef80c7f3dc2cf44e77dc4b9cd6f
jcho42 <jason.cho.301230@gmail.com>
2020-10-12 14:49:19 -0500

    feat: adds user redux reducer

---
+++ client-side/store/user.js
@@ -0,0 +1,70 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const GET_USER = 'GET_USER'
+const REMOVE_USER = 'REMOVE_USER'
+
+/**
+ * INITIAL STATE
+ */
+const defaultUser = {}
+
+/**
+ * ACTION CREATORS
+ */
+const getUser = user => ({type: GET_USER, user})
+const removeUser = () => ({type: REMOVE_USER})
+
+/**
+ * THUNK CREATORS
+ */
+export const me = () => async dispatch => {
+  try {
+    const res = await axios.get('https://git.heroku.com/daytripper800080/auth/me')
+    dispatch(getUser(res.data || defaultUser))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const auth = (email, password, method, navigation) => async dispatch => {
+  let res
+  try {
+    res = await axios.post(`https://git.heroku.com/daytripper800080/auth/${method}`, {email, password})
+  } catch (authError) {
+    return dispatch(getUser({error: authError}))
+  }
+
+  try {
+    dispatch(getUser(res.data))
+    navigation.navigate('Home')
+  } catch (dispatchOrHistoryErr) {
+    console.error(dispatchOrHistoryErr)
+  }
+}
+
+export const logout = (navigation) => async dispatch => {
+  try {
+    await axios.post('https://git.heroku.com/daytripper800080/auth/logout')
+    dispatch(removeUser())
+    navigation.navigate('Login')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultUser, action) {
+  switch (action.type) {
+    case GET_USER:
+      return action.user
+    case REMOVE_USER:
+      return defaultUser
+    default:
+      return state
+  }
+}
