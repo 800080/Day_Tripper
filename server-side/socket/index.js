101d7a64aac959fe385c71c3815690dfd9272c9a
jcho42 <jason.cho.301230@gmail.com>
2020-10-13 14:04:46 -0500

    refactor: socket for client and server

--- server-side/socket/index.js
+++ server-side/socket/index.js
@@ -1,6 +1,18 @@
 module.exports = (io) => {
   io.on("connection", (socket) => {
+    console.log('socket.rooms ---->', socket.rooms)
     console.log(`A socket connection to the server has been made: ${socket.id}`)
+
+    socket.on('room', async room => {
+      await socket.join(room)
+      console.log('socketAfterJoinRoom ---->', socket.rooms)
+    })
+
+    socket.on('leaveRoom', room => {
+      socket.leave(room, () => {
+        console.log('Left room ', room)
+      })
+    })
 
     socket.on("disconnect", () => {
       console.log(`Connection ${socket.id} has left the building`)
