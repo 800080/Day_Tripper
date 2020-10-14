4881e738615f5fe77c29adb04ae09426c6411cfb
jcho42 <jason.cho.301230@gmail.com>
2020-10-14 11:21:01 -0500

    feat: adds username to chat

--- client-side/socket.js
+++ client-side/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 import store, { getNewMessage } from './store'
 
 const socket = io("https://daytripper800080.herokuapp.com")
-// const socket = io("http://192.168.50.37:7070")
+// const socket = io("http://localhost:7070")
 
 socket.on('connect', () => {
   console.log('Connected!!!!!!!!!!!!!!!!!!!!!!')
--- server-side/api/messages.js
+++ server-side/api/messages.js
@@ -1,18 +1,17 @@
 const router = require('express').Router()
-const { ChatMessage } = require('../db/models')
+const { ChatMessage, User } = require('../db/models')
 module.exports = router
 
 //GET mounted on /api/messages
 router.get('/trip/:tripId', async (req, res, next) => {
-  console.log("MESSAGES ON API")
   try {
     const messages = await ChatMessage.findAll({
       order: [['createdAt', 'ASC']],
       where: {
         tripId: req.params.tripId
-      }
+      },
+      include: User
     })
-
     res.send(messages)
   } catch (error) {
     next(error)
@@ -22,13 +21,15 @@ router.get('/trip/:tripId', async (req, res, next) => {
 //POST mounted on /api/messages
 router.post('/trip/:tripId/user/:userId', async (req, res, next) => {
   try {
-    console.log('req.body --------->', req.body)
     const newMessage = await ChatMessage.create({
       message: req.body.message,
       tripId: req.params.tripId,
       userId: req.params.userId
     })
-    res.send(newMessage)
+    const message = await ChatMessage.findByPk(newMessage.id, {
+      include: User
+    })
+    res.send(message)
   } catch (error) {
     next(error)
   }
