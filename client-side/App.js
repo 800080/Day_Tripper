5251239c38aee3054f0619c9568e63d82a2d19f6
jcho42 <jason.cho.301230@gmail.com>
2020-10-14 21:23:11 -0500

    feat: adds react-native-paper for styling/ui on AllTrips screen

--- client-side/App.js
+++ client-side/App.js
@@ -2,30 +2,33 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
 import store from './store';
 import { Login, HomeScreen, Chat, AllTrips, SingleTrip } from './screens';
-import './socket'
+import './socket';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={() => ({
-              headerLeft: null,
-            })}
-          />
-          <Stack.Screen name="Chat" component={Chat} />
-          <Stack.Screen name="AllTrips" component={AllTrips} />
-          <Stack.Screen name="SingleTrip" component={SingleTrip} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={() => ({
+                headerLeft: null,
+              })}
+            />
+            <Stack.Screen name="Chat" component={Chat} />
+            <Stack.Screen name="AllTrips" component={AllTrips} />
+            <Stack.Screen name="SingleTrip" component={SingleTrip} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
     </Provider>
   );
 }
--- client-side/screens/AllTrips.js
+++ client-side/screens/AllTrips.js
@@ -1,44 +1,79 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
-import { connect } from 'react-redux'
-import { fetchAllTrips, fetchSingleTrip } from '../store'
+import { ScrollView, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { fetchAllTrips, fetchSingleTrip } from '../store';
+import { List } from 'react-native-paper'
 
 class AllTrips extends Component {
   constructor() {
-    super()
-    this.handleClick = this.handleClick.bind(this)
+    super();
+    this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
-    this.props.fetchAllTrips(this.props.user.id)
+    this.props.fetchAllTrips(this.props.user.id);
   }
 
   handleClick(tripId) {
-    this.props.navigation.navigate('SingleTrip')
-    this.props.fetchSingleTrip(tripId)
+    this.props.navigation.navigate('SingleTrip');
+    this.props.fetchSingleTrip(tripId);
   }
 
   render() {
-    console.log("TRIPS---", this.props.trips)
+    const upcomingTrips = this.props.trips.filter(trip => trip.userTrips[0].status === 'accepted')
+    const pendingTrips = this.props.trips.filter(trip => trip.userTrips[0].status === 'pending')
     return (
-      <View>
-        {
-          this.props.trips.map((trip) => {
-            return <Text key={trip.id} onPress={() => this.handleClick(trip.id)}>{trip.title}</Text>
+      <ScrollView>
+        <List.Section>
+          <List.Subheader style={styles.listSubheader}>Upcoming Trips</List.Subheader>
+          {
+          upcomingTrips.map((trip) => {
+            return <List.Item
+            key={trip.id}
+            title={trip.title}
+            description={trip.notes}
+            left={() => <List.Icon color="#800080" icon="airplane-takeoff" />}
+            onPress={() => this.handleClick(trip.id)}
+          />
           })
         }
-      </View >
-    )
+        </List.Section>
+        <List.Section>
+          <List.Subheader style={styles.listSubheader}>Pending Trips</List.Subheader>
+          {
+          pendingTrips.map((trip) => {
+            return <List.Item
+            key={trip.id}
+            title={trip.title}
+            description={trip.notes}
+            left={() => <List.Icon color="#800080" icon="alert-outline" />}
+            onPress={() => this.handleClick(trip.id)}
+          />
+          })
+        }
+        </List.Section>
+      </ScrollView>
+    );
   }
 }
 
 const mapState = (state) => ({
   user: state.user,
-  trips: state.trips.allTrips
-})
+  trips: state.trips.allTrips,
+});
 
 const mapDispatch = (dispatch) => ({
   fetchAllTrips: (userId) => dispatch(fetchAllTrips(userId)),
-  fetchSingleTrip: (tripId) => dispatch(fetchSingleTrip(tripId))
-})
+  fetchSingleTrip: (tripId) => dispatch(fetchSingleTrip(tripId)),
+});
 
-export default connect(mapState, mapDispatch)(AllTrips)
+export default connect(mapState, mapDispatch)(AllTrips);
+
+const styles = StyleSheet.create({
+  listSubheader: {
+    fontSize: 20,
+    color: 'white',
+    backgroundColor: '#800080',
+    alignSelf: "flex-start",
+    marginLeft: 20
+  }
+})
