6e78a3d8b09b1576cd99860a718a394eecc6d386
Andrew Gross <gross.andrewr@gmail.com>
2020-10-16 15:19:37 -0400

    feat: added logos

--- client-side/screens/Home.js
+++ client-side/screens/Home.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-import { connect } from 'react-redux'
+import { Text, View, Button, StyleSheet, Image } from 'react-native';
+import { connect } from 'react-redux';
 
 function HomeScreen({ navigation, user }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Image
+        style={styles.logo}
+        source={require('../assets/DayTripperLogo.png')}
+      />
       <Text>Welcome {user.name}!</Text>
       <Button
         title="View Trips"
         onPress={() => navigation.navigate('AllTrips')}
       />
-      {/* <Button
-        title="Go to Itin 1"
-        onPress={() => navigation.navigate('Itinerary')}
-      /> */}
     </View>
   );
 }
 
 const mapState = (state) => ({
-  user: state.user
-})
+  user: state.user,
+});
 
-export default connect(mapState)(HomeScreen)
+export default connect(mapState)(HomeScreen);
+
+const styles = StyleSheet.create({
+  logo: {
+    height: 180,
+    width: 260,
+    borderRadius: 5,
+    margin: 20
+  },
+});
